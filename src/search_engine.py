1dc9bd39515b4ed4f11f802d4ab4c5c54ff907f0
andrea-lk <andrealk@stanford.edu>
2024-06-29 11:57:18 -0400

    Create search_engine.py

---
+++ src/search_engine.py
@@ -0,0 +1,243 @@
+import os
+import sys
+import string
+
+
+def create_index(filenames, index, file_titles):
+    """
+    This function is passed:
+        filenames:      a list of file names (strings)
+
+        index:          a dictionary mapping from terms to file names (i.e., inverted index)
+                        (term -> list of file names that contain that term)
+
+        file_titles:    a dictionary mapping from a file names to the title of the article
+                        in a given file
+                        (file name -> title of article in that file)
+
+    The function will update the index passed in to include the terms in the files
+    in the list filenames.  Also, the file_titles dictionary will be updated to
+    include files in the list of filenames.
+
+    >>> index = {}
+    >>> file_titles = {}
+    >>> create_index(['test1.txt'], index, file_titles)
+    >>> index
+    {'file': ['test1.txt'], '1': ['test1.txt'], 'title': ['test1.txt'], 'apple': ['test1.txt'], 'ball': ['test1.txt'], 'carrot': ['test1.txt']}
+    >>> file_titles
+    {'test1.txt': 'File 1 Title'}
+    >>> index = {}
+    >>> file_titles = {}
+    >>> create_index(['test2.txt'], index, file_titles)
+    >>> index
+    {'file': ['test2.txt'], '2': ['test2.txt'], 'title': ['test2.txt'], 'ball': ['test2.txt'], 'carrot': ['test2.txt'], 'dog': ['test2.txt']}
+    >>> file_titles
+    {'test2.txt': 'File 2 Title'}
+    >>> index = {}
+    >>> file_titles = {}
+    >>> create_index(['test1.txt', 'test2.txt'], index, file_titles)
+    >>> index
+    {'file': ['test1.txt', 'test2.txt'], '1': ['test1.txt'], 'title': ['test1.txt', 'test2.txt'], 'apple': ['test1.txt'], 'ball': ['test1.txt', 'test2.txt'], 'carrot': ['test1.txt', 'test2.txt'], '2': ['test2.txt'], 'dog': ['test2.txt']}
+    >>> index = {}
+    >>> file_titles = {}
+    >>> create_index(['test1.txt', 'test2.txt', 'test2.txt'], index, file_titles)
+    >>> index
+    {'file': ['test1.txt', 'test2.txt'], '1': ['test1.txt'], 'title': ['test1.txt', 'test2.txt'], 'apple': ['test1.txt'], 'ball': ['test1.txt', 'test2.txt'], 'carrot': ['test1.txt', 'test2.txt'], '2': ['test2.txt'], 'dog': ['test2.txt']}
+    >>> file_titles
+    {'test1.txt': 'File 1 Title', 'test2.txt': 'File 2 Title'}
+    >>> index = {'file': ['test1.txt'], '1': ['test1.txt'], 'title': ['test1.txt'], 'apple': ['test1.txt'], 'ball': ['test1.txt'], 'carrot': ['test1.txt']}
+    >>> file_titles = {'test1.txt': 'File 1 Title'}
+    >>> create_index([], index, file_titles)
+    >>> index
+    {'file': ['test1.txt'], '1': ['test1.txt'], 'title': ['test1.txt'], 'apple': ['test1.txt'], 'ball': ['test1.txt'], 'carrot': ['test1.txt']}
+    >>> file_titles
+    {'test1.txt': 'File 1 Title'}
+    """
+    # index is a dictionary. any changes to dict made here will persist
+
+    for file in filenames:
+        with open(file) as f:
+            # stores first line of file in first_line
+            first_line = f.readline()
+
+            add_line_to_index(file, index, first_line)
+
+            for line in f:
+                add_line_to_index(file, index, line)
+
+            # put back in dict
+            file_titles[file] = first_line.strip()
+
+
+def add_line_to_index(file, index, line):
+    line = line.strip()
+    line = line.split()
+    for term in line:
+        # parse the terms
+
+        term = term.strip(string.punctuation)
+        # make it lowercase
+        term = term.lower()
+
+        # add the term to the dictionary
+
+        if term not in index:
+            index[term] = []
+        if file not in index[term]:
+            index[term].append(file)
+
+
+
+def search(index, query):
+    """
+    This function is passed:
+        index:      a dictionary mapping from terms to file names (inverted index)
+                    (term -> list of file names that contain that term)
+
+        query  :    a query (string), where any letters will be lowercase
+
+    The function returns a list of the names of all the files that contain *all* of the
+    terms in the query (using the index passed in).
+
+    >>> index = {}
+    >>> create_index(['test1.txt', 'test2.txt'], index, {})
+    >>> search(index, 'apple')
+    ['test1.txt']
+    >>> search(index, 'ball')
+    ['test1.txt', 'test2.txt']
+    >>> search(index, 'file')
+    ['test1.txt', 'test2.txt']
+    >>> search(index, '2')
+    ['test2.txt']
+    >>> search(index, 'carrot')
+    ['test1.txt', 'test2.txt']
+    >>> search(index, 'dog')
+    ['test2.txt']
+    >>> search(index, 'nope')
+    []
+    >>> search(index, 'apple carrot')
+    ['test1.txt']
+    >>> search(index, 'apple ball file')
+    ['test1.txt']
+    >>> search(index, 'apple ball nope')
+    []
+    """
+
+    word_list = query.split()
+    word1 = word_list[0]
+
+    if word1 in index:
+        #initialize if word is in index
+        overlap = index[word1]
+    else:
+        #return empty list if there is nth
+        return []
+
+    #returns list of terms
+    for word in word_list:
+        #gives me list associated with that word
+        if word in index:
+            #only make posting list if word in index
+            posting_list = index[word]
+            overlap = common(overlap, posting_list)
+        else:
+            return []
+
+    return overlap
+
+
+
+def common(list1, list2):
+    common_list = []
+    # for each element in list 1
+    for elem1 in list1:
+        # if the element is in list 2, and not already in common list, append it.
+        if elem1 in list2:
+            if elem1 not in common_list:
+                common_list.append(elem1)
+
+    return common_list
+
+
+def do_searches(index, file_titles):
+    """
+    This function is given an inverted index and a dictionary mapping from
+    file names to the titles of articles in those files.  It allows the user
+    to run searches against the data in that index.
+    """
+    while True:
+        query = input("Query (empty query to stop): ")
+        query = query.lower()                   # convert query to lowercase
+        if query == '':
+            break
+        results = search(index, query)
+
+        # display query results
+        print("Results for query '" + query + "':")
+        if results:                             # check for non-empty results list
+            for i in range(len(results)):
+                title = file_titles[results[i]]
+                print(str(i + 1) + ".  Title: " + title + ",  File: " + results[i])
+        else:
+            print("No results match that query.")
+
+
+def textfiles_in_dir(directory):
+    """
+    Given the name of a valid directory, returns a list of the .txt
+    file names within it.
+
+    Input:
+        directory (string): name of directory
+    Returns:
+        list of (string) names of .txt files in directory
+    """
+    filenames = []
+
+    for filename in os.listdir(directory):
+        if filename.endswith('.txt'):
+            filenames.append(os.path.join(directory, filename))
+
+    return filenames
+
+
+def main():
+    """
+    Usage: searchengine.py <file directory> -s
+    The first argument specified should be the directory of text files that
+    will be indexed/searched.  If the parameter -s is provided, then the
+    user can interactively search (using the index).  Otherwise (if -s is
+    not included), the index and the dictionary mapping file names to article
+    titles are just printed on the console.
+    """
+    # Get command line arguments
+    args = sys.argv[1:]
+
+    num_args = len(args)
+    if num_args < 1 or num_args > 2:
+        print('Please specify directory of files to index as first argument.')
+        print('Add -s to also search (otherwise, index and file titles will just be printed).')
+    else:
+        # args[0] should be the folder containing all the files to index/search.
+        directory = args[0]
+        if os.path.exists(directory):
+            # Build index from files in the given directory
+            files = textfiles_in_dir(directory)
+            index = {}          # index is empty to start
+            file_titles = {}    # mapping of file names to article titles is empty to start
+            create_index(files, index, file_titles)
+
+            # Either allow the user to search using the index, or just print the index
+            if num_args == 2 and args[1] == '-s':
+                do_searches(index, file_titles)
+            else:
+                print('Index:')
+                print(index)
+                print('File names -> document titles:')
+                print(file_titles)
+        else:
+            print('Directory "' + directory + '" does not exist.')
+
+
+if __name__ == '__main__':
+    main()
